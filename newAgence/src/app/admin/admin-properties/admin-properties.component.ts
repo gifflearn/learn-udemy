bea73cf51cc2b88ec7ddcdca2ee74d9aa209ac60
Jean-François LAHELLEC <jean-francois.lahellec@logisneuf.com>
2020-08-26 09:40:07 +0200

    formulaire newAgence

--- newAgence/src/app/admin/admin-properties/admin-properties.component.ts
+++ newAgence/src/app/admin/admin-properties/admin-properties.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForOf } from '@angular/common';
-import { NgForm, FormGroup } from '@angular/forms';
+import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-admin-properties',
@@ -10,19 +10,37 @@ import { NgForm, FormGroup } from '@angular/forms';
 export class AdminPropertiesComponent implements OnInit {
 
   public propertiesForm: FormGroup;
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
+
 
   ngOnInit() {
+    // Formulaire avec la methode reactive
+    this.initPropertiesForm();
   }
 
+  // Formulaire avec la methode reactive
   initPropertiesForm() {
-    this.propertiesForm = 
+    this.propertiesForm = this.formBuilder.group({
+      title: ['', Validators.required],
+      category: ['', Validators.required],
+      surface: ['', Validators.required],
+      rooms: ['', Validators.required],
+      description: '',
+      price: ['', Validators.required]
+    });
   }
-  onSubmitPropertiesForm(form: NgForm) {
-    console.log(form.value);
-    // methode template ngForm
-    const theTitle = form.value['title'];
-    console.log(theTitle);
+
+  // Si on code le formulaire avec Template
+  // onSubmitPropertiesForm(form: NgForm) {
+  //   console.log(form.value);
+  //   // methode template ngForm
+  //   const theTitle = form.value['title'];
+  //   console.log(theTitle);
+  // }
+
+  // Formulaire avec la methode reactive
+  onSubmitPropertiesForm() {
+    console.log(this.propertiesForm.value);
   }
 
 }
